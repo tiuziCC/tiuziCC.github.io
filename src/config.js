460f3b072120467d44de7e6c1c7a034fbfdb269f
chensizhe1 <chensizhesh@outlook.com>
2026-01-16 23:39:35 +0100

    Update config and dependencies

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/chandrikadeb7',
+      url: 'https://tiuziCC.github.io/',
     },
     {
       name: 'Linkedin',
