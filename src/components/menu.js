0b94432dd2433af69b3fe843e5aa556d90999e42
chensizhe1 <chensizhesh@outlook.com>
2026-01-17 02:38:10 +0100

    resume update

--- src/components/menu.js
+++ src/components/menu.js
@@ -271,7 +271,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/myresume.pdf" className="resume-link">
               Resume!
             </a>
           </nav>
